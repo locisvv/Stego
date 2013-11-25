5708fefc0d4e8a2f1bb171bfeb0d89169b0948b7
locisvv <locisvv@gmail.com>
2013-11-25 10:40:38 +0200

    finished decription

--- src/org/stego/Encrypt.java
+++ src/org/stego/Encrypt.java
@@ -1,54 +1,80 @@
 package org.stego;
 
-public class Encrypt {
-	public static void main(String[] args) {
+import java.math.BigInteger;
 
-		p(extendedEuclid(19, 11) + " ");
-		randomGenerator();
+public class Encrypt {
+	public static int p = 11;
+	public static int q = 19;
+	public static int n = p * q;
+	public static int x = 101;
+	
+	public static void main(String[] args) {
+		CryptoText cryptoText = encryption(new boolean[] {false, true, true, true, true, true, true, true, true, true, true, true, true, true, true,true, true, true, true, true});
+		printArray(cryptoText.cryptoText);
+		printArray(decryption(cryptoText.cryptoText, cryptoText.randomKey));
+	}
+	
+	public static CryptoText encryption(boolean[] text) {
+		long firstX = (x * x) % n;
+		return encryptionOrDecryption(text, firstX);
+	}
+	
+	public static boolean[] decryption(boolean[] text, long randomKey) {
+		long firstX = getX0(text.length, randomKey);
+		return encryptionOrDecryption(text, firstX).cryptoText;
+	}
+	
+	public static CryptoText encryptionOrDecryption(boolean[] text, long firstX) {
+		long random;
+		boolean item;
+		boolean[] cryptoText = text;
+		long previousItem = firstX;
+		for (int i = 0; i < cryptoText.length; i++) {
+			random = randomGenerator(previousItem);
+			previousItem = random;
+			item = (random % 2) == 1 ? true : false;
+			cryptoText[i] ^= item;
+		}
+		
+		return new CryptoText(cryptoText, previousItem);
+	}
+	
+	public static long randomGenerator(long previousItem){
+		return (previousItem * previousItem) % n;		
+	}
+	
+	public static long getX0(int m, long mItem) {
+		Euclidean params = extendedEuclid(p, q);
+		int a = params.x;
+		int b = params.y;
+		
+		double l = Math.pow(((p + 1) / 4), m) % (p -1);
+		double k = Math.pow(((q + 1) / 4), m) % (q -1);
+		
+		double u = Math.pow(mItem % p, l) % p;
+		double v = Math.pow(mItem % q, k) % q;
+		
+		return (long) (a*p*v + b*q*u) % n;
 	}
 	
 	public static Euclidean extendedEuclid(int p, int q){
-		p(p + " " + q);
 		if(q == 0){
 			return new Euclidean(p, 1, 0);
 		}
 		Euclidean e = extendedEuclid(q, p % q);
 		return new Euclidean(e.d, e.y, e.x - (p/q*e.y));
 	}
-
 	
-	public static void  randomGenerator(){
-		long[] array = new long[20];
-		int p = 11;
-		int q = 19;
-		int n = p * q;
-		int x = 101;
-		array[0] = (x * x) % n;
-				
-		for (int i = 1; i < array.length; i++) {
-			array[i] = (array[i-1] * array[i-1]) % n;
-		}
-		
-		printArray(array);
-		
-		int a = 7;
-		int b = -4;
-		int m = 13;
-
-		double l = Math.pow(((p + 1) / 4), m) % (p -1);
-		double k = Math.pow(((q + 1) / 4), m) % (q -1);
-		
-		double u = Math.pow(array[13] % p, l) % p;
-		double v = Math.pow(array[13] % q, k) % q;
-		double key = (a*p*v + b*q*u) % n;
-		
-		p(key + "");
-	}
 	public static void p(String str) {
 		System.out.println(str);
 	}
 	
 	public static void printArray(long[] array) {
+		for (int i = 0; i < array.length; i++) {
+			 p(i + " - " + array[i]);
+		}
+	}
+	public static void printArray(boolean[] array) {
 		for (int i = 0; i < array.length; i++) {
 			 p(i + " - " + array[i]);
 		}
@@ -69,7 +95,16 @@ class Euclidean{
 	@Override
 	public String toString() {
 		return "Euclidean [d=" + d + ", x=" + x + ", y=" + y + "]";
+	}	
+}
+
+class CryptoText{
+	public boolean[] cryptoText;
+	public long randomKey;
+	
+	public CryptoText(boolean[] cryptoText, long randomKey) {
+		this.cryptoText = cryptoText;
+		this.randomKey = randomKey;
 	}
-	
-	
+
 }
